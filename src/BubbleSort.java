ba27ec694233e11894a1d73b2d8655c185f8061f
Matthew Philps <philps.matthew@gmail.com>
2019-03-25 07:53:51 +0900

    set up for bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class BubbleSortExample {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[] {12, 13, 24, 10, 3, 6, 90, 70};
+
+        bubbleSort(array, 0, array.length);
+
+        System.out.println(Arrays.toString(array));
+    };
+    public static void bubbleSort(Object[] array, int fromIndex, int toIndex){
+        //TODO
+    }
+    
+}
